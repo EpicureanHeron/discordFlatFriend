e2b5c1e84ce0a07a7a39d552d8cfbddba6841580
Joe Thor <joethor1989@gmail.com>
2021-05-19 21:17:58 -0500

    cron job working, same with whatday command

--- flatfriendboy.py
+++ flatfriendboy.py
@@ -1,15 +1,31 @@
 import discord
 from discord.ext import commands
+import datetime
+import aiocron
 
 # will not play unless it is friday
 # instead rickrolls
 # auto posts flat friend friday
  
+CHANNEL_ID = open("channel.txt","r").readline()
  
 TOKEN = open("token.txt","r").readline()
 client = commands.Bot(command_prefix = '.')#answers with the ms latency
 @client.command()
-async def flatfriend(ctx):
-    await ctx.send(f'https://www.youtube.com/watch?v=A5U8ypHq3BU')
- 
+async def whatday(ctx):
+    today = datetime.datetime.today().weekday()
+    if today == 2:
+        await ctx.send(f'https://www.youtube.com/watch?v=A5U8ypHq3BU')
+    else:
+        await ctx.send(f'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+@aiocron.crontab('0 4 * * THU')
+async def cornjob1():
+    CHANNEL_ID = open("channel.txt","r").readline()
+    channel = client.get_channel(int(CHANNEL_ID))
+    print('success')
+
+    await channel.send('https://www.youtube.com/watch?v=A5U8ypHq3BU')
+
+
 client.run(TOKEN)
