7f50abef0e7f5914e0a0dbadd421794ce0f11222
Joe Thor <joethor1989@gmail.com>
2021-05-20 14:57:25 -0500

    added kyle's suggestion

--- flatfriendboy.py
+++ flatfriendboy.py
@@ -28,5 +28,11 @@ async def cornjob1():
 
     await channel.send('https://www.youtube.com/watch?v=A5U8ypHq3BU')
 
+@aiocron.crontab('30 16 * * FRI')
+async def cronjob2():
+    CHANNEL_ID = open("channel.txt","r").readline()
+    channel = client.get_channel(int(CHANNEL_ID))
+    await channel.send('https://twitter.com/CraigWeekend/status/1393340094602366976?s=20')
+
 
 client.run(TOKEN)
