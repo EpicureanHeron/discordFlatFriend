51f472f479b8badfbb3444e3d368409cb2a39672
Joe Thor <joethor1989@gmail.com>
2021-05-19 21:39:20 -0500

    updated to be ready for prodcution, need to update channel still tho

--- flatfriendboy.py
+++ flatfriendboy.py
@@ -14,13 +14,13 @@ client = commands.Bot(command_prefix = '.')#answers with the ms latency
 @client.command()
 async def whatday(ctx):
     today = datetime.datetime.today().weekday()
-    if today == 2:
+    if today == 4:
         await ctx.send('https://www.youtube.com/watch?v=A5U8ypHq3BU')
     else:
         await ctx.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
-# @aiocron.crontab('0 4 * * THU')
-@aiocron.crontab('* * * * *')
+@aiocron.crontab('0 4 * * THU')
+# @aiocron.crontab('* * * * *')
 async def cornjob1():
     CHANNEL_ID = open("channel.txt","r").readline()
     channel = client.get_channel(int(CHANNEL_ID))
