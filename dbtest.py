345db88959a5b7e8afbbdf266343df7fb6d376ed
Joe Thor <joethor1989@gmail.com>
2022-01-15 21:28:06 -0600

    database working and using pandas for analysis

--- dbtest.py
+++ dbtest.py
@@ -1,12 +1,12 @@
 from sqlalchemy import DateTime, create_engine, MetaData, Table, Column, Integer, String
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
 from models import Base, Bestfriend
 import datetime
+import pandas as pd
 
 time = datetime.datetime.now()
-user = 'EpicureanHeron'
-interaction = 'good bot'
-#import models
+
 
 
 # engine = create_engine("sqlite:///flatfriend.db")
@@ -17,7 +17,7 @@ print(engine.table_names())
 session = DBSession()
 
 
-# new_bestie = Bestfriend(username='EpicureanHeron', interaction_type='good bot', date=time)
+# new_bestie = Bestfriend(username='TheGreatBorotan', interaction_type='at replys', date=time)
 # session.add(new_bestie)
 # session.commit()
 
@@ -33,14 +33,46 @@ besties = Table(
     Column('date', DateTime)
 )
 
-# engine = create_engine('sqlite:///C:\\sqlite3\\flatfriend.db')
-
-
 conn = engine.connect()
  
 b = besties.select()
-
 result = conn.execute(b)
+# c = besties.count()
+
+# result = conn.execute(c)
+
+# print (session.query(besties).count())
+# print(conn.execute(b).keys())
+
+df = pd.DataFrame([(d.id, d.username, d.interaction_type, d.date) for d in result], 
+                  columns=['id', 'username', 'interaction_type', 'date'])
+
+df2 = df.where(df['date'] < datetime.datetime.today())
+
+print(df2)
+df2 = df2.groupby(['username']).count()
+
+
+print(df)
 
 for row in result:
    print (row)
+
+results = session.query(besties).filter(besties.c.username=='EpicureanHeron').count()
+
+print(results)
+
+# for row in results:
+#    print (row)
+# results = session.query(besties).count(besties.c.username)
+# https://stackoverflow.com/questions/46978199/select-the-count-and-value-of-a-sqlalchemy-column-using-having
+
+# q = session.query(
+#     Bestfriend.func.count(besties.c.username),
+#     besties.c.username
+# ).group_by(
+#     besties.c.username
+# ).having(
+#     Bestfriend.func.count(besties.c.username) > 1
+# )
+# print(q)
--- models.py
+++ models.py
@@ -19,11 +19,3 @@ class Bestfriend(Base):
     date=Column(DateTime)
 
 Base.metadata.create_all(engine)
-# base = declarative_base()
- 
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
