191aa93a5288d2a731cb020b5eaca02a62323fc9
Joe Thor <joe.f.thor@gmail.com>
2025-03-20 05:28:05 -0500

    Update gatorme.py

--- gatorme.py
+++ gatorme.py
@@ -34,6 +34,37 @@ def goon():
 
     return random_url_gif
 
+def crocme():
+    f = open('database.json')
+    # returns JSON object as
+    # a dictionary
+    file_content = json.load(f)
+    baseURL = 'https://api.giphy.com/v1/gifs/random?'
+    apiKey = "api_key=" +  file_content['gif_api_key']
+    tag = '&tag='
+    subj = 'crocs shoes'
+    requestURL = baseURL + apiKey + tag + subj
+    responses = requests.get(requestURL).json()
+    # pprint.pprint(responses)
+    random_url_gif =   responses['data']['images']['downsized_large']['url']
+
+    return random_url_gif
+
+def skol():
+    f = open('database.json')
+    # returns JSON object as
+    # a dictionary
+    file_content = json.load(f)
+    baseURL = 'https://api.giphy.com/v1/gifs/random?'
+    apiKey = "api_key=" +  file_content['gif_api_key']
+    tag = '&tag='
+    subj = 'Aaron Rodgers'
+    requestURL = baseURL + apiKey + tag + subj
+    responses = requests.get(requestURL).json()
+    # pprint.pprint(responses)
+    random_url_gif =   responses['data']['images']['downsized_large']['url']
+
+    return random_url_gif
 
 def yakuza():
     f = open('database.json')
@@ -43,8 +74,8 @@ def yakuza():
     baseURL = 'https://api.giphy.com/v1/gifs/random?'
     apiKey = "api_key=" +  file_content['gif_api_key']
     tag = '&tag='
-    subj = 'Kiryu'
-    requestURL = baseURL + apiKey + tag + subj + tag + 'Yakuza'
+    subj = 'like a dragon'
+    requestURL = baseURL + apiKey + tag + subj
     responses = requests.get(requestURL).json()
     # pprint.pprint(responses)
     random_url_gif =   responses['data']['images']['downsized_large']['url']
